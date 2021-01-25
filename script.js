f7b71c0c7f79d19616984d88a3258b50e7d8bdf5
Esenov-Kubanychbek <kubaesenov031005@gmail.com>
2021-01-25 19:58:22 +0000

    using for and if we wrote even numbers from 0 to 100

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ for (let number = 0; number <= 100; number++){
 for (let number = 11; number <= 33; number++){
     console.log(number)
 }
+
+
+for (let number = 0; number <= 100; number++){
+    if(number % 2 == 0){
+        console.log(number)
+    }
+}
